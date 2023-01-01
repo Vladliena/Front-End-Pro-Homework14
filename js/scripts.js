fe7fc4ea813f0d62031d894dcddb6e3c8ca1ff56
vladliena <sotnikovavladlena@gmail.com>
2023-01-01 22:20:45 +0200

    last commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 
 function findLongestWord (word){
-    let result = ''
+    let result = '';
     if (typeof word === 'string'){
         const arrayOfWords = word.split(' ')
         for (let i = 0; i < arrayOfWords.length; i++){
             if (arrayOfWords[i].length > result.length){
-                result = arrayOfWords[i]
+                result = arrayOfWords[i];
             }
         }
     } else {
-        result = 'it is not a string'
+        result = 'it is not a string';
     }
     return result
 }
 
-console.log(findLongestWord( 'london is the capital of great britain its political economic and cultural centre'))
+console.log(findLongestWord( 'london is the capital of great britain its political economic and cultural centre'));
